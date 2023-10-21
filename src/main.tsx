fd3b1c40f6ef64c61e9ca7bce04693ae41978ddd
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-20 23:00:33 -0300

    implements react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './components/theme-provider.tsx';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './components/theme-provider.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
