bb6dc941aaed58c762f2d342d99a97bd2b177ab4
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-20 22:36:33 -0300

    use auth form

---
+++ src/components/user-auth-form.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/utils';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => (
+  <div className={cn('grid gap-6', className)} {...props}>
+    <form>
+      <div className="grid gap-2">
+        <div className="grid gap-1">
+          <Label className="sr-only" htmlFor="username">
+            Username
+          </Label>
+          <Input
+            id="username"
+            placeholder="John doe"
+            autoCapitalize="none"
+            autoCorrect="off"
+          />
+        </div>
+        <Button>Sign In with Username</Button>
+      </div>
+    </form>
+  </div>
+);
