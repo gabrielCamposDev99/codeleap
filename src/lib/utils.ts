003e543461c5565d0f97159a79102eb80fc2bfe8
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-22 13:40:41 -0300

    create dynamic date formate by region

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDateByLocale(dateProvided: Date) {
+  if (!dateProvided) return 'Date not provided';
+  const locale = navigator.language;
+  const date = new Date(dateProvided);
+  const options = {
+    dateStyle: 'short',
+    timeStyle: 'short',
+  } as Intl.DateTimeFormatOptions;
+  const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
+  return formattedDate;
+}
