319dc9e1197f6d9f9939f9e447db2d2145bcde36
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-22 15:06:53 -0300

    mobile nav

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,3 +1,4 @@
+import { Menu } from 'lucide-react';
 import { ModeToggle } from '@/components/mode-toggle';
 import {
   Card, CardContent, CardHeader, CardTitle
@@ -9,6 +10,11 @@ import { usePosts } from '@/actions/api/postsApi/usePosts';
 import List from '@/components/list';
 import { PostType } from '@/validation/interfaces/IPost';
 import { CreatePostForm } from '@/components/forms/post/create-post-form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 const PostsView = () => {
   const { posts } = usePosts();
@@ -22,10 +28,27 @@ const PostsView = () => {
                 CodeLeap Network
               </h2>
             </a>
-            <div className="ml-auto flex items-center space-x-4">
-              <ModeToggle />
 
-              <UserNav />
+            <div className="ml-auto items-center space-x-4">
+              <div className="flex sm:hidden">
+                <Popover>
+                  <PopoverTrigger>
+                    <Menu className="h-10 w-10" />
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <div className="flex flex-col items-center space-y-4">
+                      <ModeToggle />
+                      <UserNav />
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
+              <div className="hidden sm:flex items-center space-x-4">
+                <ModeToggle />
+                <div className="hidden sm:flex">
+                  <UserNav />
+                </div>
+              </div>
             </div>
           </div>
         </div>
