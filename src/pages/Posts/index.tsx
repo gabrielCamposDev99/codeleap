1edf6e89aeb335ec97870a12fd27fa7b1a58ee7e
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-22 13:40:55 -0300

    implements list posts

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -5,39 +5,48 @@ import {
 import { UserNav } from '@/components/user-nav';
 
 import { PostCard } from '@/components/post-card';
-import { PostForm } from '@/components/post-form';
+import { usePosts } from '@/actions/api/postsApi/usePosts';
+import List from '@/components/list';
+import { PostType } from '@/validation/interfaces/IPost';
+import { CreatePostForm } from '@/components/forms/post/create-post-form';
 
-const PostsView = () => (
-  <div>
-    <div className=" flex-col md:flex bg-primary hover:bg-primary/90">
-      <div className="container">
-        <div className="flex min-h-16 items-center px-4">
-          <h2 className="py-2 text-3xl font-bold tracking-tight text-slate-100">
-            CodeLeap Network
-          </h2>
-          <div className="ml-auto flex items-center space-x-4">
-            <ModeToggle />
+const PostsView = () => {
+  const { posts } = usePosts();
+  return (
+    <div>
+      <div className=" flex-col md:flex bg-primary hover:bg-primary/90">
+        <div className="container">
+          <div className="flex min-h-16 items-center px-4">
+            <h2 className="py-2 text-3xl font-bold tracking-tight text-slate-100">
+              CodeLeap Network
+            </h2>
+            <div className="ml-auto flex items-center space-x-4">
+              <ModeToggle />
 
-            <UserNav />
+              <UserNav />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="container flex-col mt-6">
-      <Card>
-        <CardHeader>
-          <CardTitle>What’s on your mind?</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <PostForm />
-        </CardContent>
-      </Card>
-    </div>
+      <div className="container flex-col mt-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>What’s on your mind?</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CreatePostForm />
+          </CardContent>
+        </Card>
+      </div>
 
-    <div className="container flex-col mt-6">
-      <PostCard />
+      <div className="container flex-col mt-6">
+        <List
+          data={posts?.results}
+          render={(post: PostType) => <PostCard {...post} key={post.id} />}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PostsView;
