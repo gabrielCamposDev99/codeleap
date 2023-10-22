0713d8d2cb65b9d7e2d8519c05290511d71849c8
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-22 13:41:06 -0300

    post type

--- src/validation/interfaces/IPost.ts
+++ src/validation/interfaces/IPost.ts
@@ -1,4 +1,9 @@
 import { z } from 'zod';
 import { postSchema } from '../schemas/post-schema';
 
-export type PostType = z.infer<typeof postSchema>;
+export type CreatePostType = Omit<PostType, 'id' | 'created_datetime'>;
+
+export type PostType = z.infer<typeof postSchema> & {
+  id: number;
+  created_datetime: Date;
+};
