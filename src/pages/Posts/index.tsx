8f8e28a998aa4ad62629847474d66c377837de2e
Gabriel Campos <dev.gabrielscampos@gmail.com>
2023-10-22 14:51:23 -0300

    redirect to codeleap

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -17,9 +17,11 @@ const PostsView = () => {
       <div className=" flex-col md:flex bg-primary hover:bg-primary/90">
         <div className="container">
           <div className="flex min-h-16 items-center px-4">
-            <h2 className="py-4 text-3xl font-bold tracking-tight text-slate-100">
-              CodeLeap Network
-            </h2>
+            <a href="https://codeleap.co.uk/" target="_blank" rel="noreferrer">
+              <h2 className="py-4 text-3xl font-bold tracking-tight text-slate-100">
+                CodeLeap Network
+              </h2>
+            </a>
             <div className="ml-auto flex items-center space-x-4">
               <ModeToggle />
 
